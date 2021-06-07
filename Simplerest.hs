4a6de96e3eece20b719740e6eeb0a27ad6d475f6
Michal Makowski <makosek@poczta.onet.pl>
2021-06-07 23:08:24 +0200

    idk

--- Simplerest.hs
+++ Simplerest.hs
@@ -2,8 +2,9 @@
 {-# LANGUAGE GADTs, DataKinds, KindSignatures, StandaloneDeriving, TypeFamilies, TypeOperators, ScopedTypeVariables, TypeApplications, AllowAmbiguousTypes, TemplateHaskell, QuasiQuotes, PolyKinds, FlexibleContexts #-}
 {-# LANGUAGE UndecidableInstances #-} -- TODO Meh
 
--- import Data.Type.Equality
--- import GHC.TypeLits
+import Data.Type.Equality
+import GHC.TypeLits
+-- import Data.Proxy
 
 data PNat :: * where
     I :: PNat
@@ -38,3 +39,26 @@ broadcast ROne (a :> as) (V b) = (a, b) :> broadcast ROne as (V b)
 -- TODO i don't like these being here, but oh well
 broadcast LOne (V a) (V b) = V (a, b)
 broadcast ROne (V a) (V b) = V (a, b)
+
+oneEq :: Broadcasts I n m -> n :~: m
+oneEq Equal = Refl
+oneEq LOne = Refl
+oneEq ROne = Refl
+
+eq :: Broadcasts (S m) (S n) k -> m :~: n
+eq Equal = Refl
+
+-- TODO this needs to be constrained somehow, and that needs... constraints, right?
+-- getBroadcast :: Vec m a -> Vec n b -> Broadcasts m n k -- (Broadcasts m n k, Vec k (a, b))
+getBroadcast :: CanBroadcast m n ~ k => SPNat m -> SPNat n -> Broadcasts m n k
+getBroadcast SI _ = LOne
+getBroadcast _ SI = ROne
+getBroadcast (SS m@(SS _)) (SS n@(SS _)) = Equal
+
+type family CanBroadcast (m :: PNat) (n :: PNat) :: PNat where
+    CanBroadcast n n = n
+    CanBroadcast I n = n
+    CanBroadcast n I = n
+    CanBroadcast m n = TypeError (Text "‘CanBroadcast’ didn't match"
+                         :$$: Text "when applied to the types ‘"
+                         :<>: ShowType m :<>: Text "’ and ‘" :<>: ShowType n :<>: Text "’") 
